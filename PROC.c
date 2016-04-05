f46a69288694100310789f2984cdc354eb334bc0
jaimemontoya <webmaster@jaimemontoya.com>
2016-04-04 20:17:52 -0400

    Update PROC.c

--- PROC.c
+++ PROC.c
@@ -24,12 +24,16 @@ int get_opcode(uint32_t hs) {
     return (hs >> 26);
 }
 
+int get_function(uint32_t hs) {
+    hs = hs << 26;
+    return (hs >> 26);
+}
 
 int main(int argc, char * argv[]) {
-  
+
     int MaxInst = 0;
     int status = 0;
-    uint32_t i; 
+    uint32_t i;
     uint32_t PC,newPC;
     uint32_t CurrentInstruction;
 
@@ -43,11 +47,11 @@ int main(int argc, char * argv[]) {
     initHeap();
     initFDT();
     initRegFile(0);
-    
+
     //Load required code portions into Emulator Memory
     status =  LoadOSMemory(argv[1]);
     if(status <0) { return status; }
-    
+
     //set Global & Stack Pointers for the Emulator
     // & provide startAddress of Program in Memory to Processor
     write_initialization_vector(exec.GSP, exec.GP, exec.GPC_START);
@@ -57,55 +61,96 @@ int main(int argc, char * argv[]) {
     PC = exec.GPC_START;
     for(i=0; i<MaxInst ; i++) {
         DynInstCount++;
-        CurrentInstruction = readWord(PC,true);  
+        CurrentInstruction = readWord(PC,true);
         printRegFile();
     /********************************/
     //Add your implementation here
     /********************************/
 
         uint32_t opcode = get_opcode(CurrentInstruction);
+	uint32_t functionvalue = get_function(CurrentInstruction);
+	
+	printf("Iteration:%i\n", i);
+	printf("The value of opcode is:%zu\n", opcode);
+	printf("The value of function is:%zu\n", functionvalue);
 
         switch(opcode) {
             case 0:                     // SPECIAL = 0
-            
+	        switch(functionvalue) {
+		    case 32: // ADD
+		    break;
+		    case 34: // SUB
+I		    break;
+		    case 24: // MULT
+		    break;
+		}
+		break;
             case 1:                     // REGIMM = 1
-
+		break;
             case 2:                     // J
+		break;
             case 3:                     // JAL
+		break;
             case 4:                     // BEQ
+		break;
             case 5:                     // BNE
+		break;
             case 6:                     // BLEZ
+		break;
             case 7:                     // BGTZ
+		break;
             case 8:                     // ADDI
+		break;
             case 9:                     // ADDIU
+		break;
             case 10:                    // SLTI
+		break;
             case 11:                    // SLTIU
+		break;
             case 12:                    // ANDI
+		break;
             case 13:                    // ORI
+		break;
             case 14:                    // XORI
+		break;
             case 15:                    // LUI
+		break;
             case 20:                    // BEQL
+		break;
             case 21:                    // BNEL
+		break;
             case 22:                    // BLEZL
+		break;
             case 32:                    // LB
+		break;
             case 33:                    // LH
+		break;
             case 34:                    // LWL
+		break;
             case 35:                    // LW
+		break;
             case 36:                    // LBU
+		break;
             case 37:                    // LHU
+		break;
             case 38:                    // LWR
+		break;
             case 40:                    // SB
+		break;
             case 41:                    // SH
+		break;
             case 42:                    // SWL
+		break;
             case 43:                    // SW
+		break;
             case 46:                    // SWR
-
+		break;
         }
 
         PC = PC + 4;
     } //end fori
-    
-    
+
+
     //Close file pointers & free allocated Memory
     closeFDT();
     CleanUp();
